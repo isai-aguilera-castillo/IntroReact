5eb3aa1c7163bc5e2b8a36b4e4369b8e3ac6685b
ISAI ARON AGUILERA CASTILLO <isaiaron@hotmail.com>
2022-05-05 20:58:23 -0600

    mostrar que sigue el siguiente guajador

--- src/index.js
+++ src/index.js
@@ -38,8 +38,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
+    return (
+      // el resto no ha cambiado
+      
     return (
       <div>
         <div className="status">{status}</div>
