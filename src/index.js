83d7e961680f83d624234e3b6bd53e80fc4f3676
ISAI ARON AGUILERA CASTILLO <isaiaron@hotmail.com>
2022-05-05 20:37:01 -0600

    modificacion de la calse square

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class Board extends React.Component {
   class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={function() { console.log('click'); }}>
           {this.props.value}
         </button>
       );
